261352bf55ca064154ebedb07e441fd196f10c33
harold <1051180226@qq.com>
2020-04-21 20:51:14 +0800

    ok

--- spring-boot-consumer/src/main/java/com/example/demo/service/controller/SampleController.java
+++ spring-boot-consumer/src/main/java/com/example/demo/service/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.controller;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.example.demo.service.dto.RequestDto;
 import com.example.demo.service.generator.DubboServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,26 +35,23 @@ public class SampleController {
         /**
          * 解析方法参数 methodParams
          */
-        int size = maps.size();
-        int i = 0;
-        if (maps != null) {
-            for (i = 1; maps.get("param_" + i) != null; i++) {
-                Map map = new HashMap();
-                map.put("ParamType", maps.get("ParamType_" + i));
-                map.put("param", maps.get("param_" + i));
-                paramInfos.add(map);
+        int j=0;
+        for(j=1; j<= (maps.size()/2 ); j++)
+        {
+            String type = (String)maps.get("ParamType_" + j);
+            Object value = maps.get("param_" + j);
+            
+            if(StringUtils.isBlank(type) || null == value)
+            {
+                continue;
             }
+            
+            Map map = new HashMap();
+            map.put("ParamType", type);
+            map.put("param",value );
+            paramInfos.add(map);  
         }
-
-        /**
-         * 参数顺序校验，应对param_1,param_2,param_6这种情况，
-         * 这些对参数的校验到时候封装一个Util类;
-         */
-
-
-        if (i - size != 1) {
-            return "参数序列错误";
-        }
+        
 
         return dubboServiceFactory.genericInvoke(requestDto.getInterfaceName(), requestDto.getMethodName(), paramInfos);
         //return null;
--- spring-boot-consumer/src/main/java/com/example/demo/service/generator/DubboServiceFactory.java
+++ spring-boot-consumer/src/main/java/com/example/demo/service/generator/DubboServiceFactory.java
@@ -6,11 +6,10 @@ import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.utils.ReferenceConfigCache;
 import com.alibaba.dubbo.rpc.service.GenericService;
 import com.example.demo.service.config.DubboConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 /**
@@ -32,7 +31,8 @@ public class DubboServiceFactory {
     @Autowired
     DubboConfig dubboConfig;
 
-    public Object genericInvoke(String interfaceClass, String methodName, List<Map<String, Object>> parameters) throws ClassNotFoundException {
+    public Object genericInvoke(String interfaceClass, String methodName, List<Map<String, Object>> parameters)
+            throws ClassNotFoundException {
 
         ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
         application.setName(dubboConfig.getName());
@@ -42,13 +42,13 @@ public class DubboServiceFactory {
         reference.setInterface(interfaceClass); // 接口名
         reference.setGeneric(true); // 声明为泛化接口
 
-        /*ReferenceConfig实例很重，封装了与注册中心的连接以及与提供者的连接，
-        需要缓存，否则重复生成ReferenceConfig可能造成性能问题并且会有内存和连接泄漏。
-        API方式编程时，容易忽略此问题。
-        这里使用dubbo内置的简单缓存工具类进行缓存*/
+        /*
+         * ReferenceConfig实例很重，封装了与注册中心的连接以及与提供者的连接， 需要缓存，否则重复生成ReferenceConfig可能造成性能问题并且会有内存和连接泄漏。
+         * API方式编程时，容易忽略此问题。 这里使用dubbo内置的简单缓存工具类进行缓存
+         */
 
 
-        //GenericService genericService = reference.get();//当时用postman测试，每访问一次，消耗的内存就增加好几兆，吓到宝宝了,所以不要用这种方式
+        // GenericService genericService = reference.get();//当时用postman测试，每访问一次，消耗的内存就增加好几兆，吓到宝宝了,所以不要用这种方式
 
 
         ReferenceConfigCache cache = ReferenceConfigCache.getCache();
@@ -57,42 +57,18 @@ public class DubboServiceFactory {
 
         int len = parameters.size();
 
+        String[] invokeParamTypes = new String[len];
         Object[] invokeParams = new Object[len];
         for (int i = 0; i < len; i++) {
+            invokeParamTypes[i] = (String) parameters.get(i).get("ParamType");
             invokeParams[i] = parameters.get(i).get("param");
         }
-
-        /**
-         * 获取方法所有参数类型
-         */
-        Class<?> testClass = Class.forName(interfaceClass);
-        Method[] methods = testClass.getDeclaredMethods();
-        Class<?>[] invokeParamClass = null;
-        for (Method method : methods) {
-            if (methodName.equals(method.getName())) {
-                invokeParamClass = method.getParameterTypes();
-                break;
-            }
-        }
-
-        if (invokeParamClass == null) {
-            return "方法不存在";
-        }
-        if (invokeParamClass.length != len) {
-            return "方法参数错误";
-        }
-
-        String[] invokeParamTypes = new String[invokeParamClass.length];
-        for (int i = 0; i < invokeParamClass.length; i++) {
-            invokeParamTypes[i] = invokeParamClass[i].getName();
-        }
+       
 
         return genericService.$invoke(methodName, invokeParamTypes, invokeParams);
 
     }
 
 }
-
-
 
 
